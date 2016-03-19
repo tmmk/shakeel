6d7e91490581b7e1bf051b3e6c553b26d500e9cb
Tejen Hasmukh Patel <tpatel2@mail.ccsf.edu>
2016-03-18 21:55:19 -0700

    Set up UIPickerView for picking shipping options/prices

--- shakeel/ChooseShippingViewController.swift
+++ shakeel/ChooseShippingViewController.swift
@@ -8,19 +8,43 @@
 
 import UIKit
 
-class ChooseShippingViewController: UIViewController {
+class ChooseShippingViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var pickerData = [
+        "March 21-22   $250",
+        "March 23-26   $200",
+        "March 27-31   $175",
+        "April 1-15   $150",
+    ];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        print("picker view did load");
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 4;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row];
+    }
 
     /*
     // MARK: - Navigation
