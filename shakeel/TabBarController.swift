a0daaea4b5816ed0ad590a95a456df9a88d5a770
Tejen Hasmukh Patel <tpatel2@mail.ccsf.edu>
2016-03-18 21:55:01 -0700

    Set tab bar to open Home (middle) tab by default when App opens.

---
+++ shakeel/TabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarController.swift
+//  shakeel
+//
+//  Created by Tejen Hasmukh Patel on 3/18/16.
+//  Copyright © 2016 ccsf. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        selectedIndex = 1;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
